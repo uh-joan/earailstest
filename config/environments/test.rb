d484263630bda37b16d96b22901c73a03f6e7eaf
uh-joan <j.saez-pons@herts.ac.uk>
2014-12-09 19:11:51 +0000

    initial commit

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -30,6 +30,10 @@ Rails.application.configure do
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = { host: 'example.com' }
+
+  # Randomize the order test cases are executed
+  config.active_support.test_order = :random
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141208181344) do
+ActiveRecord::Schema.define(version: 20141209163944) do
 
   create_table "product_add_on_list_items", force: true do |t|
     t.integer  "user_id"
@@ -116,6 +116,16 @@ ActiveRecord::Schema.define(version: 20141208181344) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
+    t.string   "remember_digest"
+    t.boolean  "admin",             default: false
+    t.string   "activation_digest"
+    t.boolean  "activated",         default: false
+    t.datetime "activated_at"
+    t.string   "reset_digest"
+    t.datetime "reset_sent_at"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
